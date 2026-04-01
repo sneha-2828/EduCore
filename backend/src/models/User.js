089a9eb01fc2c06318adaeb70184378bfef7c6e7
ratnapollasnehagoud <your-github-ratnapollasnehagoud@gmail.com>
2026-04-01 22:50:26 +0530

    Created User model with authentication fields and schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    email: { type: String, required: true, unique: true },
+
+    password: { type: String, required: true },
+
+    role: {
+      type: String,
+      enum: ["student", "faculty", "admin"],
+      default: "student",
+    },
+
+    facultyId: {
+      type: String,
+      sparse: true,
+      unique: true,
+    },
+
+    // 🔥 ADD THIS
+    semester: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Semester",
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "approved",
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
