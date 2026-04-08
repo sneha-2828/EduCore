a9f03febcf746fe21aee091cdd6432ab1312805d
sneha-2828 <ratnapollasnehagoud@gmail.com>
2026-04-08 20:26:44 +0530

    Update userController.js

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -11,6 +11,7 @@
 //       return res.status(400).json({ message: "User already exists" });
 //     }
 
+
 //     const hashedPassword = await bcrypt.hash(password, 10);
 
 //     const faculty = await User.create({
