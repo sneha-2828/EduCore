6466ed0bb64d45a8e0220542d70e3da4ef88fa91
sneha-2828 <ratnapollasnehagoud@gmail.com>
2026-04-11 23:05:20 +0530

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+
 // ===============================
 // REGISTER
 // ===============================
